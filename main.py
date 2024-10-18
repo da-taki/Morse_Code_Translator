ea07443ea5e965a68a32846d3b5dc826549168cb
da-taki <singhtaknoor@gmail.com>
2024-10-18 20:11:23 +0530

    Adding sounds

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+#Importing libraries
+import winsound
+import time
+
 # Making a dictionary where letters and digits are keys and the 
 # values are their respective translation in morse morse_code
 morse_code = {"a" : ".-",
@@ -52,14 +56,30 @@ morse_code = {"a" : ".-",
 
 #getting the input from the user
 mssg = input("Enter the line which you want to translate to morse code: ")
-
+try:
+    voice = int(input("Do you want to hear how this morse codw will sound(1 for yes 0 for no)?: "))
+except ValueError:
+    print("Please enter either 1 or 0!")
+    voice = 0
 #turing the message into lower case
 mssg = mssg.lower()
+
+#fucntion for playing beeps
+def play_sound(ms_code):
+    for symbol in ms_code:
+        if symbol == ".":
+            winsound.Beep(1000,450) #Dot sound
+        if symbol == "-":
+            winsound.Beep(1000,900) #Dash sound
+        time.sleep(0.2)
 
 #using a for loop to translate the input into morse code 
 for i in mssg:
     if morse_code.get(i) == None:
         continue
     else:
+        ms_code = morse_code[i]
         print(morse_code[i], end=" ")
-
+        if voice == 1:
+            play_sound(ms_code)
+        time.sleep(0.8)
